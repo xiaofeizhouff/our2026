09bf4f3d878277f378c8b69f1e117b358f48f830
xiaofeizhouff <xiaofeizhou396@gmail.com>
2026-09-25 23:12:28 +0800

    style: unify profile visual stack

--- components/profile/MemoryDaysBubble.tsx
+++ components/profile/MemoryDaysBubble.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export function MemoryDaysBubble({ days, years }: { days: number; years: number }) {
-  return <div className="days-bubble-anchor"><div className="days-bubble"><Image src="/illustrations/profile/message-bubble-cropped.png" alt="手绘绿色对话框" width={1200} height={550} sizes="(max-width: 430px) 96vw, 410px" preload unoptimized /><p>原来我们已经认识 <b>{days}</b> 天了。<br/>这是我们认识的第 {years} 年。</p></div></div>;
+  return <div className="days-bubble"><Image src="/illustrations/profile/message-bubble-cropped.png" alt="手绘绿色对话框" width={1200} height={550} sizes="(max-width: 430px) 81vw, 343px" preload unoptimized /><p>原来我们已经认识 <b>{days}</b> 天了。<br/>这是我们认识的第 {years} 年。</p></div>;
 }
