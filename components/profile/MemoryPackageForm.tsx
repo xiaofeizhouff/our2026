1c8b8e58225ab335648154782a5f94223d4a623b
xiaofeizhouff <xiaofeizhou396@gmail.com>
2026-09-25 22:36:17 +0800

    style: refine profile form layout

--- components/profile/MemoryPackageForm.tsx
+++ components/profile/MemoryPackageForm.tsx
@@ -5,9 +5,11 @@ type Props = { info: BasicInfo; onChange: (info: BasicInfo) => void };
 export function MemoryPackageForm({ info, onChange }: Props) {
   return <div className="package-form">
     <img src="/illustrations/profile/package-cropped.png" alt="手绘回忆包裹" className="package-art" />
-    <p className="package-logo">MEMORY EXPRESS</p>
     <div className="package-slip">
-      <p className="shipping-subtitle">回忆快递单 · 2026</p>
+      <div className="shipping-head">
+        <p className="shipping-title">MEMORY EXPRESS</p>
+        <p className="shipping-subtitle">回忆快递单 · 2026</p>
+      </div>
       <div className="my-2 border-t border-dashed border-[#1f3155]" />
       <label>收件人 / NAME<input value={info.creatorName} onChange={e => onChange({ ...info, creatorName: e.target.value })} placeholder="写下你的名字" /></label>
       <label>寄出日期 / SINCE<input type="date" value={info.metDate} onChange={e => onChange({ ...info, metDate: e.target.value })} /></label>
