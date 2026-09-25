916ee9249df1cf9a9ca29d7211036c8da5ee248c
xiaofeizhouff <xiaofeizhou396@gmail.com>
2026-09-25 21:40:45 +0800

    style: replace profile art with transparent png assets

--- components/profile/MemoryDaysBubble.tsx
+++ components/profile/MemoryDaysBubble.tsx
@@ -1,3 +1,3 @@
 export function MemoryDaysBubble({ days, years }: { days: number; years: number }) {
-  return <div className="days-bubble"><img src="/illustrations/profile/message-bubble.jpg" alt="手绘绿色对话框" /><p>原来我们已经认识 <b>{days}</b> 天了。<br/>这是我们认识的第 {years} 年。</p></div>;
+  return <div className="days-bubble"><img src="/illustrations/profile/message-bubble.png" alt="手绘绿色对话框" /><p>原来我们已经认识 <b>{days}</b> 天了。<br/>这是我们认识的第 {years} 年。</p></div>;
 }
